bcb3a71788c862883722fd6e2083ff51de683385
Mahendra <mahendravakkalagadda@gmail.com>
2025-06-04 14:18:34 +0530

    Add files via upload

---
+++ portfolio/javascript.js/contact.js
@@ -0,0 +1,2 @@
+let g=alert("Please on desktop mode in your mobile for better Experience ");
+console.log(g);
---
+++ portfolio/javascript.js/index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Software Engineer","Frontend Developer" , " Web Developer",],
+    typespeed: 10,
+    backspeed: 100,
+    backDelay: 1500,
+    loop: true
+
+});
